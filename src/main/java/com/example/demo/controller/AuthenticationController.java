2a08eb78a803cfcec0ee848cf2a17f1052a09ae4
sontung04 <119484670+sontung04@users.noreply.github.com>
2025-09-05 16:28:38 +0700

    Revert "Order class"

--- src/main/java/com/example/demo/controller/AuthenticationController.java
+++ src/main/java/com/example/demo/controller/AuthenticationController.java
@@ -6,7 +6,10 @@ import com.example.demo.dto.RegisterRequest;
 import com.example.demo.service.AuthenticationService;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
